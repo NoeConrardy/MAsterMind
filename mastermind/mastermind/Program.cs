c33220eecf91fd650a6d7b5f804cd3cb420fdbec
NoeConrardy <91312969+NoeConrardy@users.noreply.github.com>
2021-12-02 15:52:04 +0100

    suite mastermind

---
+++ mastermind/mastermind/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mastermind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pionRouge = 0;
+            int pionBlanc = 0;
+                      
+            Console.WriteLine("Entrez la couleur de votre ch
+            couleurRandom(out int[] programColorsArray);oix");
+            string playerColors = Console.ReadLine();
+            playerColorToArray(playerColors, out int[] playerColorsArray);
+            tableauComparatif(programColorsArray, out int[] programColorsArray_)
+
+        }
+
+        //genere des couleurs aux hasards
+        static void couleurRandom(out int[] programColorsArray)
+        {
+            combi = new int[4];
+            for (int i = 0; i <= 3; i++)
+            {
+                Random rnd = new Random();
+                //int valeur = rnd.Next(1, 7);
+                combi[i] = rnd.Next(1, 7);
+
+            }
+        }
+        /*static void Afficher(int[] combi)
+        {
+            for (int i = 0; i <= 3; i++)
+            {
+                Console.Write(combi[i]);
+
+            }
+            Console.ReadLine();
+        }*/
+
+
+        static void playerColorToArray(string reponseColors, out int[] playerColorsArray)
+        {
+            playerColorsArray = new int[4];
+            for (int i = 0; i <= 3; i++)
+            {
+                playerColorsArray[i] = int.Parse(reponseColors[i].ToString());
+            }
+        }
+
+        //calculer les pions rouges
+        static void calculePionRouge(int[] programColorsArray_, int[] playerColorsArray, out int pionRouge)
+        {
+            pionRouge = 0;
+            for (int i = 0; i <= 3; i++)
+            {
+                if (programColorsArray_[i] == playerColorsArray[i])
+                {
+                    programColorsArray_[i] = -1;
+                    playerColorsArray[i] = -2;
+                    pionRouge++ 
+                }
+
+            }
+        }
+
+        //calculer les pions blancs 
+        static void calculePionBlanc(int[] programColorsArray_, int[] playerColorsArray, out int pionBlanc)
+        {
+            pionBlanc = 0;
+            for (int i = 0; i <= 3; i++)
+            {
+
+            }
+
+        }
+
+        //tableau comparatif qui ajoute de dans.
+        static void tableauComparatif(int[] programColorsArray, out int[] programColorsArray_)
+        {
+            programColorsArray_ = new int[4];
+            for (int i = 0; i <= 3; i++)
+            {
+                programColorsArray_[i] = programColorsArray[i];
+            }
+        }
+    }
+}
